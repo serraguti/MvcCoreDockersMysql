af3f1f2bde828bc739ec88e8054c98eee2e9df01
Serra <pacoserranox@gmail.com>
2022-02-02 16:16:41 +0100

    commit

---
+++ MvcCoreDockersMysql/Controllers/DepartamentosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreDockersMysql.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreDockersMysql.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.repo.GetDepartamentos());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(this.repo.FindDepartamento(id));
+        }
+    }
+}
