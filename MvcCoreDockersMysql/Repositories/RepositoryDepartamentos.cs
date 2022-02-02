37a252c8488744169d0b1f1657b42a9c25db74d0
Serra <pacoserranox@gmail.com>
2022-02-02 16:11:37 +0100

    commit

---
+++ MvcCoreDockersMysql/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,29 @@
+﻿using MvcCoreDockersMysql.Data;
+using MvcCoreDockersMysql.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreDockersMysql.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Departamento> GetDepartamentos()
+        {
+            return this.context.Departamentos.ToList();
+        }
+
+        public Departamento FindDepartamento(int id)
+        {
+            return this.context.Departamentos.SingleOrDefault(x => x.IdDepartamento == id);
+        }
+    }
+}
