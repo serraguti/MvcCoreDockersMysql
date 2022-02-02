7102abf7800706b31659b095c37760f807e8eced
Serra <pacoserranox@gmail.com>
2022-02-02 16:05:05 +0100

    Add project files.

---
+++ MvcCoreDockersMysql/Data/DepartamentosContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreDockersMysql.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreDockersMysql.Data
+{
+    public class DepartamentosContext: DbContext
+    {
+        public DepartamentosContext(DbContextOptions<DepartamentosContext> options)
+            : base(options) { }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
